18c1622689c0f9b378fed077c1eba7bdef068988
Pawel Pudlo <pawelzlatan98@interia.pl>
2024-11-16 14:29:55 +0100

    Lab 2 12%

---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
@@ -0,0 +1,27 @@
+package com.capgemini.wsb.fitnesstracker.training.internal;
+
+import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Kontroler obsługujący operacje na treningach.
+ * Umożliwia dodawanie, usuwanie oraz pobieranie treningów.
+ */
+@RestController
+@RequestMapping("/v2")
+@RequiredArgsConstructor
+class TrainingController {
+
+    private final TrainingServiceImpl trainingService;
+    private final TrainingMapper trainingMapper;
+
+
+    @GetMapping(value = "/getAllTraining")
+    public List<TrainingDto> getAllTraining() {
+        return trainingService.getAllTrainings().stream().map(trainingMapper::toDto).toList();
+    }
+
+}
--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingRepository.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingRepository.java
@@ -1,8 +1,22 @@
 package com.capgemini.wsb.fitnesstracker.training.internal;
 
 import com.capgemini.wsb.fitnesstracker.training.api.Training;
+import com.capgemini.wsb.fitnesstracker.user.api.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+
 interface TrainingRepository extends JpaRepository<Training, Long> {
+
+    default List<Training> getAllTrainings() {
+        return findAll()
+                .stream()
+                .toList();
+
+
+    }
 
 }
